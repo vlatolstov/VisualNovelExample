e796f97024a43e563f094bcfc103d8b0eab00d65
Vladimir Tolstov <vlatolstov@gmail.com>
2025-06-02 13:22:36 +0400

    Revert "Temp commit"
    This reverts commit c31a9ceb0d98093e9e57a20791f9eb2c9e80662d.

--- Assets/Runtime/Minigame/IMiniGameService.cs
+++
@@ -1,7 +0,0 @@
-using System.Threading;
-
-using Naninovel;
-public interface IMiniGameService : IEngineService {
-    bool WasLastGameSuccessful { get; }
-    UniTask PlayAsync(CancellationToken cancellationToken = default);
-}
